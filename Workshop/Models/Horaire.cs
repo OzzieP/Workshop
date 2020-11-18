d61ba5c51639fcc3440fb0bbc0be86c4d79bf6f8
Opineppes <43174074+Opineppes@users.noreply.github.com>
2020-11-18 09:43:28 +0100

    Ajout des models de la base de données

---
+++ Workshop/Models/Horaire.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workshop.Models
+{
+    public class Horaire
+    {
+        public int idHoraire { get; set; }
+        public DateTime temps { get; set; }
+    }
+}
