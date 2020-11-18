b147894b18f9edb7468c3a10aab0eda72a445cae
Francois CAHIER <francois.cahier@epsi.fr>
2020-11-18 17:55:22 +0100

    Modification des indices de jours

--- Workshop/Controllers/HomeController.cs
+++ Workshop/Controllers/HomeController.cs
@@ -70,13 +70,13 @@ namespace Workshop.Controllers
         public ActionResult Information()
         {
             DatabaseHelper database = new DatabaseHelper();
-            List<Etat> lundi = database.SelectNombreVoitureParVoie(0);
-            List<Etat> mardi = database.SelectNombreVoitureParVoie(1);
-            List<Etat> mercredi = database.SelectNombreVoitureParVoie(2);
-            List<Etat> jeudi = database.SelectNombreVoitureParVoie(3);
-            List<Etat> vendredi = database.SelectNombreVoitureParVoie(4);
-            List<Etat> samedi = database.SelectNombreVoitureParVoie(5);
-            List<Etat> dimanche = database.SelectNombreVoitureParVoie(6);
+            List<Etat> lundi = database.SelectNombreVoitureParVoie(1);
+            List<Etat> mardi = database.SelectNombreVoitureParVoie(2);
+            List<Etat> mercredi = database.SelectNombreVoitureParVoie(3);
+            List<Etat> jeudi = database.SelectNombreVoitureParVoie(4);
+            List<Etat> vendredi = database.SelectNombreVoitureParVoie(5);
+            List<Etat> samedi = database.SelectNombreVoitureParVoie(6);
+            List<Etat> dimanche = database.SelectNombreVoitureParVoie(0);
             ViewBag.Message = "Page information.";
             ViewBag.lundi = lundi;
             ViewBag.mardi = mardi;
