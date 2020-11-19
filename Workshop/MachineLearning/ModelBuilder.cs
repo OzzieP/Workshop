b671c48032a102a5ca3452a563e4dda13b6b2044
Brendan LEGENDRE <brendan.legendre@epsi.fr>
2020-11-19 16:01:17 +0100

    ML connecté à une DB SQL Server et fonctionnel. Prévisions du nombre de passages toutes les heures, sur une journée et un feu donnés.

--- Workshop/MachineLearning/ModelBuilder.cs
+++ Workshop/MachineLearning/ModelBuilder.cs
@@ -18,17 +18,21 @@ namespace Workshop.MachineLearning
     {
         private static MLContext _mlContext = new MLContext();
 
+        private static string _connectionString = "Data Source=146.59.229.11;Initial Catalog=Workshop;User ID=admin;Password=EPSIworkshop2020*";
 
-        public static void CreateModel()
+
+        public static void CreateModel(int idFeu, int numWeek)
         {
             string rootDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../"));
-            string modelPath = Path.Combine(rootDir, "MLModel.zip");
+            string modelPath = Path.Combine(rootDir, "Data", "MLModel.zip");
 
-            DatabaseHelper helper = new DatabaseHelper();
-            string query = "SELECT e.jour, f.matricule, SUM(e.nbPassant) as nombre FROM etat e INNER JOIN feu f ON e.idFeu = f.idFeu GROUP BY f.matricule, e.jour";
+            string query = "SELECT f.matricule AS Feu, CAST(e.nbPassant as REAL) AS NbPassants, CAST(e.jour as REAL) AS Jour, CAST(e.numWeek as REAL) as Semaine " +
+                "FROM etat e " +
+                "INNER JOIN feu f ON e.idFeu = f.idFeu " +
+                $"WHERE e.idFeu = {idFeu} AND e.numWeek = {numWeek}";
 
             DatabaseLoader loader = _mlContext.Data.CreateDatabaseLoader<ModelInput>();
-            DatabaseSource databaseSource = new DatabaseSource(SqlClientFactory.Instance, helper.Builder.ConnectionString, query);
+            DatabaseSource databaseSource = new DatabaseSource(SqlClientFactory.Instance, _connectionString, query);
 
             IDataView dataView = loader.Load(databaseSource);
             IDataView firstWeekData = _mlContext.Data.FilterRowsByColumn(dataView, "Jour", upperBound: 1);
@@ -37,67 +41,42 @@ namespace Workshop.MachineLearning
             var forecastingPipeline = _mlContext.Forecasting.ForecastBySsa(
                 outputColumnName: "ForecastedPassants",
                 inputColumnName: "NbPassants",
-                windowSize: 7,
-                seriesLength: 4,
-                trainSize: 28,
-                horizon: 7,
+                windowSize: 24,
+                seriesLength: 1440,
+                trainSize: 10080,
+                horizon: 24,
                 confidenceLevel: 0.95f,
                 confidenceLowerBoundColumn: "LowerBoundPassants",
                 confidenceUpperBoundColumn: "UpperBoundPassants");
 
             SsaForecastingTransformer forecaster = forecastingPipeline.Fit(firstWeekData);
 
-            Evaluate(nextWeekData, forecaster, _mlContext);
-
             var forecastEngine = forecaster.CreateTimeSeriesEngine<ModelInput, ModelOutput>(_mlContext);
             forecastEngine.CheckPoint(_mlContext, modelPath);
 
-            Forecast(nextWeekData, 7, forecastEngine, _mlContext);
+            Forecast(nextWeekData, 24, forecastEngine, _mlContext);
         }
 
-        static void Evaluate(IDataView testData, ITransformer model, MLContext mlContext)
-        {
-            IDataView predictions = model.Transform(testData);
-
-            IEnumerable<int> actual = mlContext.Data.CreateEnumerable<ModelInput>(testData, true).Select(observed => observed.NbPassants);
-            IEnumerable<int> forecast = mlContext.Data.CreateEnumerable<ModelOutput>(predictions, true).Select(prediction => prediction.ForecastedPassants[0]);
-            var metrics = actual.Zip(forecast, (actualValue, forecastValue) => actualValue - forecastValue);
-
-            var MAE = metrics.Average(error => Math.Abs(error)); // Mean Absolute Error
-            var RMSE = Math.Sqrt(metrics.Average(error => Math.Pow(error, 2))); // Root Mean Squared Error
-
-            //Console.WriteLine("Evaluation Metrics");
-            //Console.WriteLine("---------------------");
-            //Console.WriteLine($"Mean Absolute Error: {MAE:F3}");
-            //Console.WriteLine($"Root Mean Squared Error: {RMSE:F3}\n");
-        }
-
-        static void Forecast(IDataView testData, int horizon, TimeSeriesPredictionEngine<ModelInput, ModelOutput> forecaster, MLContext mlContext)
+        static List<FeuForecast> Forecast(IDataView testData, int horizon, TimeSeriesPredictionEngine<ModelInput, ModelOutput> forecaster, MLContext mlContext)
         {
             ModelOutput forecast = forecaster.Predict();
 
-            IEnumerable<string> forecastOutput = mlContext.Data.CreateEnumerable<ModelInput>(testData, reuseRowObject: false)
+            IEnumerable<FeuForecast> forecastOutput = mlContext.Data.CreateEnumerable<ModelInput>(testData, reuseRowObject: false)
                 .Take(horizon)
                 .Select((ModelInput passants, int index) =>
                 {
-                    string jour = Enum.GetName(typeof(DayOfWeek), passants.Jour);
-                    int actualPassants = passants.NbPassants;
+                    string jour = Enum.GetName(typeof(DayOfWeek), (int)passants.Jour);
+                    float semaine = passants.Semaine;
+                    float actualPassants = passants.NbPassants;
                     float lowerEstimate = Math.Max(0, forecast.LowerBoundPassants[index]);
                     float estimate = forecast.ForecastedPassants[index];
                     float upperEstimate = forecast.UpperBoundPassants[index];
-                    return $"Date: {jour}\n" +
-                    $"Actual Rentals: {actualPassants}\n" +
-                    $"Lower Estimate: {lowerEstimate}\n" +
-                    $"Forecast: {estimate}\n" +
-                    $"Upper Estimate: {upperEstimate}\n";
+                    FeuForecast feu = new FeuForecast(jour, semaine, actualPassants, lowerEstimate, estimate, upperEstimate);
+
+                    return feu;
                 });
 
-            //Console.WriteLine("Rental Forecast");
-            //Console.WriteLine("---------------------");
-            //foreach (var prediction in forecastOutput)
-            //{
-            //    Console.WriteLine(prediction);
-            //}
+            return forecastOutput.ToList();
         }
     }
 }
